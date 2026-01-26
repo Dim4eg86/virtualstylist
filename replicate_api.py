07e6a3aa46b87bcdd75d5e5d440ec946a01c2103
Dim4eg86 <tsibulko.d@gmail.com>
2026-01-26 18:04:57 +0200

    Update replicate_api.py

--- replicate_api.py
+++ replicate_api.py
@@ -2,9 +2,11 @@ import replicate
 import os
 
 async def generate_vton_image(human_url, garment_url):
-    # API токен будет браться из переменных окружения Railway
+    # API токен берется из переменных окружения Railway
+    model_version = "subhash25rawat/flux-vton:a02643ce418c0e12bad371c4adbfaec0dd1cb34b034ef37650ef205f92ad6199"
+    
     output = await replicate.async_run(
-        "subhash25rawat/flux-vton:a02643ce418c0e12bad371c4adbfaec0dd1cb34b034ef37650ef205f92ad6199",
+        model_version,
         input={
             "image": human_url,
             "garment": garment_url,
@@ -12,4 +14,5 @@ async def generate_vton_image(human_url, garment_url):
             "vton_mode": "Overall"
         }
     )
-    return output
+    # Если вывод - список, берем первый элемент
+    return output[0] if isinstance(output, list) else output
